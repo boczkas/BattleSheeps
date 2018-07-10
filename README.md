8dfc0ed36eff7f0912c7fb8a121450318ae501f8
xKoem <kosmi.bartek@gmail.com>
2018-07-10 14:24:57 +0200

    afterDemo: readme updated with demo results, deployment: run.sh updated with REST

--- README.md
+++ README.md
@@ -3,3 +3,12 @@
 **This project is well known battle ship game**
 
 *To play this game you can simply run `runAll.sh` script*
+
+
+*Demos*
+-
+29.06 - passed - realistic requirements
+
+10.07 - passed - optimistic requirements
+- plus for system design (check systemDesign.jpg file)
+- plus for usage of screen
--- runAll.sh
+++ runAll.sh
@@ -9,3 +9,5 @@ screen -dmS ShotHandler java -jar ShotHandler/target/ShotHandler-1.0.jar
 screen -dmS PlayingStateMachine java -jar PlayingStateMachine/target/PlayingStateMachine-1.0.jar
 
 screen -dmS BoardHandler java -jar BoardHandler/target/BoardHandler-1.0.jar
+
+screen -dmS REST java -jar REST/target/REST-0.0.1-SNAPSHOT.jar
