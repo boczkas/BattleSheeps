69552611aaa8bbc7393fa1ffb18bca28fbddae92
Jaroslaw Slaby <jaroslaw.slaby@gmail.com>
2018-07-13 15:56:19 +0200

    configuration: Added marker interface for config confirmation and classes for specified microservices

---
+++ BattleSheepLib/src/main/java/pl/jakubowskiprzemyslaw/tajgertim/models/confirmation/FleetPlacementConfirmation.java
@@ -0,0 +1,5 @@
+package pl.jakubowskiprzemyslaw.tajgertim.models.confirmation;
+
+public class FleetPlacementConfirmation implements Confirmation {
+    private static final long serialVersionUID = 2962962549746137252L;
+}
---
+++ BattleSheepLib/src/main/java/pl/jakubowskiprzemyslaw/tajgertim/models/confirmation/GameConfigurationConfirmation.java
@@ -0,0 +1,5 @@
+package pl.jakubowskiprzemyslaw.tajgertim.models.confirmation;
+
+public class GameConfigurationConfirmation implements Confirmation {
+    private static final long serialVersionUID = -3421689741413565442L;
+}
