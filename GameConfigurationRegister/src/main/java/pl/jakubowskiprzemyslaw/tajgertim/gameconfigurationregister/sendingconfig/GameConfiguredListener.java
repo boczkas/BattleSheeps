649b964a334559bb7770253fc5e981d0cc71dd6b
Jaroslaw Slaby <jaroslaw.slaby@gmail.com>
2018-07-16 11:11:12 +0200

    gameConfigRegister: Removed unnecessary logger from GameConfiguredListener class

--- GameConfigurationRegister/src/main/java/pl/jakubowskiprzemyslaw/tajgertim/gameconfigurationregister/sendingconfig/GameConfiguredListener.java
+++ GameConfigurationRegister/src/main/java/pl/jakubowskiprzemyslaw/tajgertim/gameconfigurationregister/sendingconfig/GameConfiguredListener.java
@@ -6,25 +6,21 @@ import org.springframework.stereotype.Component;
 import pl.jakubowskiprzemyslaw.tajgertim.models.confirmation.Confirmation;
 import pl.jakubowskiprzemyslaw.tajgertim.models.confirmation.GameConfigurationConfirmation;
 import pl.jakubowskiprzemyslaw.tajgertim.queues.Queues;
-import pl.jakubowskiprzemyslaw.tajgertim.services.LoggerService;
 import pl.jakubowskiprzemyslaw.tajgertim.services.QueueService;
 
 @Component
 public class GameConfiguredListener implements ApplicationListener<GameRegisteredEvent> {
 
     private final QueueService queueService;
-    private final LoggerService loggerService;
 
     @Autowired
-    public GameConfiguredListener(QueueService queueService, LoggerService loggerService) {
+    public GameConfiguredListener(QueueService queueService) {
         this.queueService = queueService;
-        this.loggerService = loggerService;
     }
 
     @Override
     public void onApplicationEvent(GameRegisteredEvent event) {
         Confirmation gameConfigured = new GameConfigurationConfirmation();
         queueService.sendObjectToQueue(Queues._5GameReadyValidationQueue, gameConfigured);
-        loggerService.logInfo(GameConfiguredListener.class, "Eloszka");
     }
 }
