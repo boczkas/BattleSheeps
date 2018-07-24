c0926cd2a3e2b65765e5f8772b1f983632a95121
xKoem <kosmi.bartek@gmail.com>
2018-07-24 14:15:22 +0200

    removed unnecessary modules

--- BattleSheepLib/src/main/java/pl/jakubowskiprzemyslaw/tajgertim/models/shoot/PlayerShootResult.java
+++ BattleSheepLib/src/main/java/pl/jakubowskiprzemyslaw/tajgertim/models/shoot/PlayerShootResult.java
@@ -1,17 +1,13 @@
 package pl.jakubowskiprzemyslaw.tajgertim.models.shoot;
-
 import pl.jakubowskiprzemyslaw.tajgertim.models.QueueObject;
 import pl.jakubowskiprzemyslaw.tajgertim.models.player.Player;
+
+import java.util.Objects;
 
 public class PlayerShootResult implements QueueObject {
     private static final long serialVersionUID = -1338260362408236054L;
     private Player player;
     private ShootResult shootResult;
-
-    public PlayerShootResult() {
-        this.player = new Player("", "");
-        this.shootResult = ShootResult.MISS;
-    }
 
     public PlayerShootResult(Player player, ShootResult shootResult) {
         this.player = player;
@@ -32,5 +28,19 @@ public class PlayerShootResult implements QueueObject {
                 "player=" + player +
                 ", shootResult=" + shootResult +
                 '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        PlayerShootResult result = (PlayerShootResult) o;
+        return Objects.equals(player, result.player) &&
+                shootResult == result.shootResult;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(player, shootResult);
     }
 }
