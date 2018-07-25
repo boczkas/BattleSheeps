2c296cd8c5276775a4ab179ab34beb5131b49629
Koem <xKoem@users.noreply.github.com>
2018-07-25 14:40:00 +0200

    Rest pull request (#139)
    * rest: Prepared for pull request
    
    * removed unnecessary modules
    
    * Review: fixes after Adrian's review

---
+++ REST/src/main/java/pl/jakubowskiprzemyslaw/REST/models/EmptyFields.java
@@ -0,0 +1,40 @@
+package pl.jakubowskiprzemyslaw.REST.models;
+
+import pl.jakubowskiprzemyslaw.tajgertim.models.coordinates.Coordinate;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class EmptyFields {
+
+    private List<Coordinate> emptyCoordinates;
+
+    public EmptyFields(int boardSize) {
+        emptyCoordinates = new ArrayList<>();
+        fillListWithCoordinates(boardSize);
+    }
+
+    public Coordinate getRandomCoordinate() {
+        Random random = new Random();
+        int i = random.nextInt(emptyCoordinates.size());
+        return emptyCoordinates.get(i);
+    }
+
+    public boolean isCoordinateAvailable(Coordinate coordinate) {
+        return emptyCoordinates.contains(coordinate);
+    }
+
+    public void removeCoordinate(Coordinate coordinate) {
+        emptyCoordinates.remove(coordinate);
+    }
+
+    private void fillListWithCoordinates(int boardSize) {
+
+        for (int i = 0; i < boardSize; i++) {
+            for (int j = 0; j < boardSize; j++) {
+                emptyCoordinates.add(new Coordinate(i,j));
+            }
+        }
+    }
+}
