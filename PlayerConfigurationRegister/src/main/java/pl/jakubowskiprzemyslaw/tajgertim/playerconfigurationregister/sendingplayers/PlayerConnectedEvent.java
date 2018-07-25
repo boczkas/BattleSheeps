5af8f1ea4b176576aced58639dd76d21cbe5ec42
Koem <xKoem@users.noreply.github.com>
2018-07-25 11:55:01 +0200

    Player Configuration Register pull request (#133)
    * playerConfigurationRegister: Prepared for pull request
    
    * removed unnecessary modules
    
    * Review: changes after Krzysiek's review

---
+++ PlayerConfigurationRegister/src/main/java/pl/jakubowskiprzemyslaw/tajgertim/playerconfigurationregister/sendingplayers/PlayerConnectedEvent.java
@@ -0,0 +1,34 @@
+package pl.jakubowskiprzemyslaw.tajgertim.playerconfigurationregister.sendingplayers;
+
+import org.springframework.context.ApplicationEvent;
+import pl.jakubowskiprzemyslaw.tajgertim.models.configuration.PlayerConfiguration;
+
+import java.util.Objects;
+
+class PlayerConnectedEvent extends ApplicationEvent {
+
+    private static final long serialVersionUID = 570102554291869796L;
+    private final PlayerConfiguration playerConfiguration;
+
+    PlayerConnectedEvent(Object source, PlayerConfiguration playerConfiguration) {
+        super(source);
+        this.playerConfiguration = playerConfiguration;
+    }
+
+    PlayerConfiguration getPlayerConfiguration() {
+        return playerConfiguration;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        PlayerConnectedEvent that = (PlayerConnectedEvent) o;
+        return Objects.equals(playerConfiguration, that.playerConfiguration);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(playerConfiguration);
+    }
+}
---
+++ PlayerConfigurationRegister/src/main/java/pl/jakubowskiprzemyslaw/tajgertim/playerconfigurationregister/sendingplayers/PlayerRegisterConfiguration.java
@@ -0,0 +1,34 @@
+package pl.jakubowskiprzemyslaw.tajgertim.playerconfigurationregister.sendingplayers;
+
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import pl.jakubowskiprzemyslaw.tajgertim.services.LoggerService;
+import pl.jakubowskiprzemyslaw.tajgertim.services.QueueService;
+import pl.jakubowskiprzemyslaw.tajgertim.services.SessionService;
+
+@Configuration
+class PlayerRegisterConfiguration {
+
+  private final RabbitTemplate rabbitTemplate;
+
+  @Autowired
+  PlayerRegisterConfiguration(RabbitTemplate rabbitTemplate) {
+    this.rabbitTemplate = rabbitTemplate;
+  }
+
+  @Bean
+  QueueService getQueueService() {
+    return new QueueService(rabbitTemplate);
+  }
+
+  @Bean
+  SessionService getSessionService() {
+    return new SessionService();
+  }
+
+  @Bean
+  LoggerService getLoggerService() { return new LoggerService();}
+
+}
