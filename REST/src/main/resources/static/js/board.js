29074a25e48cac843ee592819e68a23c7a0b5af4
Jaroslaw Slaby <jaroslaw.slaby@gmail.com>
2018-07-27 10:35:47 +0200

    frontTurns: added prototype of turns in frontend

--- REST/src/main/resources/static/js/board.js
+++ REST/src/main/resources/static/js/board.js
@@ -1,9 +1,18 @@
 var myBoard = document.getElementById("myBoard");
 var opponentBoard = document.getElementById("opponentBoard");
+var timerIntervalID;
+var myRound = true;
+var stompClient = null;
 
 window.onload = function load() {
     setDivsForBoard(myBoard, false, true);
     setDivsForBoard(opponentBoard, true, false);
+    getUserName();
+    connect();
+};
+
+window.onunload = function close() {
+    disconnect();
 };
 
 function setCellValue(cell, j, i) {
@@ -46,6 +55,8 @@ function setCellAttribute(cell, attribute, myBoard) {
     }
 }
 
+var timerDiv = document.getElementById("timer");
+
 function clickMe() {
     var coordinates = this.getAttribute("value");
 
@@ -54,6 +65,7 @@ function clickMe() {
         url: "playing",
         data: {guiCoordinates: coordinates}
     });
+    switchRound();
 }
 
 function refresh() {
@@ -78,4 +90,87 @@ function setShotResults(container) {
         var elementById = document.getElementById(key);
         elementById.innerText = container[key];
     }
+}
+
+function startTimer(duration, display) {
+    var timerVal = duration, minutes, seconds;
+    timerIntervalID = setInterval(function () {
+        minutes = parseInt(timerVal / 60, 10);
+        seconds = parseInt(timerVal % 60, 10);
+
+        minutes = minutes < 10 ? "0" + minutes : minutes;
+        seconds = seconds < 10 ? "0" + seconds : seconds;
+
+        display.textContent = minutes + ":" + seconds;
+
+
+
+        if (--timerVal < 0) {
+            switchBoolean();
+            switchRound();
+            timerVal = duration;
+        }
+    }, 1000);
+}
+
+function stopTimer() {
+    clearInterval(timerIntervalID);
+}
+
+function switchBoolean() {
+    if(myRound === true)
+        myRound = false;
+    else
+        myRound = true;
+}
+
+function switchRound() {
+
+    if(myRound === true) {
+        myRound = false;
+        console.log("my round");
+        stopTimer();
+        //enable clicking
+        startTimer(120, timerDiv);
+    }
+    else {
+        myRound = true;
+        console.log("not my round");
+        stopTimer();
+        //disable clicking
+        startTimer(120, timerDiv);
+    }
+}
+
+
+var userName;
+
+function connect() {
+    var socket = new SockJS('/timer-websocket');
+    stompClient = Stomp.over(socket);
+    stompClient.connect('guest', 'guest', function (frame) {
+        console.log('Connected: ' + frame);
+        stompClient.subscribe('/synchro/timer/' + userName, function (synchronize) {
+            console.log(synchronize);
+            stopTimer();
+            startTimer(120, timerDiv);
+        });
+    });
+}
+
+function getUserName() {
+    $.ajax({
+        type: 'GET',
+        url: 'getname',
+        success: function (data) {
+           userName = data;
+        }
+    });
+}
+
+function disconnect() {
+    if (stompClient !== null) {
+        stompClient.disconnect();
+    }
+    console.log("Disconnected");
 }
