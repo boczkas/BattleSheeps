5ac2afcb35e83f94ec98ea8a1f8e4cbd81984dc2
Przemek <przemyslawjakubowski@gmail.com>
2018-07-27 15:39:31 +0200

    Rounds: Changes after Jarek's review

--- BattleSheepLib/src/main/java/pl/jakubowskiprzemyslaw/tajgertim/models/player/PlayingPlayer.java
+++ BattleSheepLib/src/main/java/pl/jakubowskiprzemyslaw/tajgertim/models/player/PlayingPlayer.java
@@ -2,6 +2,8 @@ package pl.jakubowskiprzemyslaw.tajgertim.models.player;
 
 import pl.jakubowskiprzemyslaw.tajgertim.models.QueueObject;
 
+import java.util.Objects;
+
 public class PlayingPlayer implements QueueObject {
 
     private static final long serialVersionUID = -3479665015132509447L;
@@ -9,6 +11,20 @@ public class PlayingPlayer implements QueueObject {
 
     public PlayingPlayer(Player player) {
         this.player = player;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        PlayingPlayer that = (PlayingPlayer) o;
+        return Objects.equals(player, that.player);
+    }
+
+    @Override
+    public int hashCode() {
+
+        return Objects.hash(player);
     }
 
     @Override
